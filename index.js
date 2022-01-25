2197cb243c42d01db9cf417cf83b9e1ccbca7046
petra-tuskan <tuskanpetra129@gmail.com>
2022-01-25 19:06:45 +0100

    write date and time

--- index.js
+++ index.js
@@ -1,44 +1,74 @@
 function newDate() {
-const date = new Date();
-const time = document.querySelector('#greet');
-time.textContent = `Good morning, it's currently ${date}`
+    const date = new Date().toLocaleString('en-GB', {
+        weekday: 'long',
+        day: 'numeric',
+        year: 'numeric',
+        month: '2-digit'
+    });
+    const clock = new Date().toLocaleString('en-GB', {
+        hourCycle: 'h24',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    });
+
+const clockSelect = document.querySelector('#clock');
+const greet = document.querySelector('#greet');
+const time = document.querySelector('#date');
+
+greet.innerHTML = `Good morning!`;
+time.innerHTML = date;
+clockSelect.innerHTML = clock;
 }
 
-setInterval(function() {
-    newDate()
-}, 1000);
+setInterval(newDate, 1000);
 
 
 
 
-async function getLocation() {
-    const response = await fetch("htps://api.ipgeolocation.io/ipgeo?apiKey=aff34ca6044f4a67bf6024e149a826e3");
+
+
+
+/*async function getLocation() {
+    const response = await fetch("https://api.ipgeolocation.io/ipgeo?apiKey=aff34ca6044f4a67bf6024e149a826e3");
     const data = await response.json();
     console.log(data);
     console.log(data.continent_name, data.country_capital)
     console.log()
 
     const city = document.querySelector('#city');
+    const city_weather = document.querySelector('#city_weather');
     
-    city.textContent = `In ${data.continent_name}/${data.country_capital}`
+    city.textContent = `In ${data.continent_name}/${data.country_capital}`;
+    city_weather.textContent = data.country_capital;
 }
 
-getLocation();
+getLocation();*/
 
-
-async function getWeather() {
-    const response = await fetch('http://api.weatherapi.com/v1/current.json?key=bce5781048194d4fbad170845222401&q=Split&aqi=no');
+/*const position = navigator.geolocation.getCurrentPosition();
+console.log(position)*/
+/*async function getWeather(getLocation) {
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=bce5781048194d4fbad170845222401&q=Zagreb&aqi=no`);
     const data = await response.json();
     console.log(data);
     console.log(data.current.temp_c)
 
     const temperature = document.querySelector('#temperature');
     temperature.textContent = `${data.current.temp_c}°C`;
-    const city_weather = document.querySelector('#city_weather');
-    city_weather.innerHTML = this.city
+    
 
 }
-getWeather();
+getWeather();*/
+
+/*async function quotes()
+{
+  const response = await fetch('https://zenquotes.io/api/quotes');
+  const data = await response.json();
+  console.log(data);
+}
+
+quotes();*/
+
 
 
 
