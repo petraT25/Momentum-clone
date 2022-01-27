5414956c431b3398226ffeeb97de6d8b105149da
petra-tuskan <tuskanpetra129@gmail.com>
2022-01-27 03:11:03 +0100

    write switch statement and greetFunc

--- index.js
+++ index.js
@@ -6,27 +6,53 @@ function newDate() {
         month: '2-digit'
     });
     const clock = new Date().toLocaleString('en-GB', {
-        hourCycle: 'h24',
+        hourCycle: 'h23',
         hour: '2-digit',
         minute: '2-digit',
         second: '2-digit'
     });
 
-const clockSelect = document.querySelector('#clock');
-const greet = document.querySelector('#greet');
-const time = document.querySelector('#date');
+    
+    
+    const time = document.querySelector('#date');
+    const clockDiv = document.querySelector('#clock');
 
-greet.innerHTML = `Good morning!`;
-time.innerHTML = date;
-clockSelect.innerHTML = clock;
+    time.innerHTML = date;
+    clockDiv.innerHTML = clock;
+    
 
-console.log(date, clock);
+    console.log(clock[0], clock[1]);
 }
 
+console.log(new Date().toString());
 setInterval(newDate, 1000);
 
+function greetFunc() { 
+let slicedTime = new Date().toString().slice(22, 24)
+console.log(slicedTime);
 
-/*async function getWeather() {
+const greet = document.querySelector('#greet');
+//greet.innerHTML = ``;
+
+
+switch(slicedTime) {
+    case '00':
+    case '01':
+    case '02':
+        console.log('evening');
+        greet.innerHTML = 'Good evening!'
+    break;
+
+    case '03':
+        console.log('nottt working');
+        greet.innerHTML = 'Morning'
+}
+}
+
+setInterval(greetFunc, 1000);
+
+
+async function getWeather() {
     const api_key = 'bce5781048194d4fbad170845222401'
     const url = `http://api.weatherapi.com/v1/current.json?key=${api_key}&q=auto:ip&aqi=no`;
     const response = await fetch(url);
@@ -35,21 +61,45 @@ setInterval(newDate, 1000);
 
     const location = document.querySelector('#city_weather');
     const temperature = document.querySelector('#temperature');
-    const icon = document.querySelector('.icon');
+    const description = document.querySelector('.description');
 
     location.innerHTML = `${data.location.name}, ${data.location.country}`;
     temperature.innerHTML = `${data.current.temp_c}°C`;
-    icon.innerHTML = `${data.current.condition.text}, ${data.current.condition.icon}`;
+    description.innerHTML = `${data.current.condition.text}`;
 
     const iconImg = `https:${data.current.condition.icon}`
 }
 
-getWeather()*/
+getWeather()
 
 async function getIcons() {
     const response = await fetch('https://www.weatherapi.com/docs/weather_conditions.json');
-    const data 
+    const data = await response.json();
+    console.log(data);
 }
+
+getIcons()
+
+async function getSaying() {
+    const response = await fetch('https://api.quotable.io/random');
+    const data = await response.json();
+    console.log(data);
+    console.log(data.content);
+    console.log(data.author);
+
+    const saying = document.querySelector('#saying_paragraph');
+    const author = document.querySelector('#author');
+    saying.innerHTML = `<q>${data.content}</q>`;
+    author.innerHTML = `-- ${data.author}`;
+
+    
+}
+
+getSaying();
+
+
+
+
 
 
 
